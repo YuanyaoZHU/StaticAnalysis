0348942c9af8ad80e1ceec7739851247e098d558
YuanyaoZHU <zhuyuanyao_only@hotmail.com>
2024-10-17 17:35:49 +0800

    matlab

--- matlabCode/eigen_calculation.m
+++ matlabCode/eigen_calculation.m
@@ -6,11 +6,12 @@ rho = 1025; %海水密度
 
 filename = "..\HydroDynamicData\Spar.1";
 fileID = fopen(filename);
-DataIn = textscan(fileID,repmat('%f',[1,5]),'CollectOutput',1); 
+DataIn = textscan(fileID,repmat('%f',[1,5]),'CollectOutput',1);
 fclose(fileID);
 Data = DataIn{1,1};
 
-M0 = load("..\HydroDynamicData\mass_total.txt")*1; % 相对平台自身质心的质量矩阵, mass.txt为平台质量矩阵，mass_total.txt为整体质量矩阵
+M0 = load("..\HydroDynamicData\mass.txt")*1; % 相对平台自身质心的质量矩阵, mass.txt为平台质量矩阵，mass_total.txt为整体质量矩阵
+M_total = load("..\HydroDynamicData\mass_total.txt");
 C0 = load("..\HydroDynamicData\Spar.hst"); % 读取静水回复力参数
 C_mooring = load("..\HydroDynamicData\mooring_force.txt");
 
@@ -21,15 +22,16 @@ xg = MC_platform(1); % 质心x坐标
 yg = MC_platform(2); % 质心y坐标
 zg = MC_platform(3); % 质心z坐标
 
-M = Inertial_Process(M0,MC_platform); % 运用移轴定理将参考点移到水线面中心处
-m = M(1,1);
+M_platform = Inertial_Process(M0,MC_platform); % 运用移轴定理将参考点移到水线面中心处
+m_platform = M_platform(1,1);
 g = 9.81;
 
-%{
+
 M_rotor_origin = zeros(6,6);
 M_rotor_origin(1,1) = 110e+3;
 M_rotor_origin(2,2) = 110e+3;
 M_rotor_origin(3,3) = 110e+3;
+m_rotor = 110e+3;
 MC_rotor = [0,0,90];
 M_rotor = Inertial_Process(M_rotor_origin,MC_rotor);
 
@@ -37,14 +39,35 @@ M_nacelle_origin = zeros(6,6);
 M_nacelle_origin(1,1) = 240e+3;
 M_nacelle_origin(2,2) = 240e+3;
 M_nacelle_origin(3,3) = 240e+3;
+m_nacelle = 240e+3;
 MC_nacelle = [0,0,90];
 M_nacelle = Inertial_Process(M_nacelle_origin,MC_nacelle);
 
 M_tower_origin = zeros(6,6);
-M_nacelle_origin(1,1) = 240e+3;
-M_nacelle_origin(2,2) = 240e+3;
-M_nacelle_origin(3,3) = 240e+3;
-%}
+M_tower_origin(1,1) = 249718;
+M_tower_origin(2,2) = 249718;
+M_tower_origin(3,3) = 249718;
+M_tower_origin(4,4) = 119076259.7;
+M_tower_origin(5,5) = 119076259.7;
+m_tower = 249718;
+MC_tower = [0,0,43.4];
+M_tower = Inertial_Process(M_tower_origin,MC_tower);
+
+panduan = 0; % panduan为1时，表示质量矩阵根据转子、机舱、塔筒等分项进行计算获得，
+             % 非1时，表示通过外部直接输入获得。
+
+if panduan == 1
+    M = M_platform + M_rotor + M_nacelle + M_tower;
+    m = m_platform + m_rotor + m_nacelle + m_tower;
+    MC = (m_platform*MC_platform + m_rotor*MC_rotor + m_nacelle*MC_nacelle + m_tower*MC_tower)/m;
+else
+    M = M_total;
+    MC = [0,0,-73];
+    m = M(1,1);
+end
+
+
+
 
 C1 = zeros(6,6); % 将读取的静水回复力参数转变为矩阵表达形式
 [p,~] = size(C0(:,1));
@@ -52,9 +75,12 @@ for i = 1:p
     C1(C0(i,1),C0(i,2)) = C0(i,3)*rho*g;    
 end
 C2 = zeros(6,6);
-C2(4,4) = -m*g*zg;
-C2(5,5) = -m*g*zg;
+C2(4,4) = -m*g*MC(3);
+C2(5,5) = -m*g*MC(3);
 C2(6,6) = 98340000;
+C_mooring(1,1)=C_mooring(1,1)*0.5;
+C_mooring(2,2)=C_mooring(2,2)*0.5;
+
 C_str = C1+C2+C_mooring;
 
 
